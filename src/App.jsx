a378992f39d198a80b124c7f3db67cf78ddba65d
yoonhe <yhehe228@gmail.com>
2021-12-22 10:36:54 +0900

    Feat : [퀴즈 시작 페이지] 퀴즈 시작 페이지 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
 export default function App() {
-  return <div>APP</div>;
+  return (
+    <div>
+      <p>지금부터 랜덤 퀴즈 풀이를 시작합니다</p>
+      <p>준비가 되셨다면 아래의 버튼을 클릭해 주세요!</p>
+      <button>퀴즈 풀기</button>
+    </div>
+  );
 }
